60dc6008cfc7e9ac29f551bf991c1a5849da6b33
Ignacio <ignaciobrenas@gmail.com>
2026-02-17 11:47:39 +0100

    TarjetasBanco (Creacion de interfaz)

---
+++ src/main/java/Controller/TarjetasBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package Controller;
+
+/**
+ *
+ * @author ignac
+ */
+public class TarjetasBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/View/Operation.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package View;
+
+/**
+ *
+ * @author ignac
+ */
+public interface Operation {
+public void pagar(double monto);
+public double consultarSaldo();
+    
+}
